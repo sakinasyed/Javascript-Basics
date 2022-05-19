c1a79885479d29989269bf0fa39bd2bea434bd13
sakinasyed <sakinasyed05@gmail.com>
2022-05-19 09:27:38 -0700

    my file 10

---
+++ 10.Return_the_percentage_of_a_number.js
@@ -0,0 +1,9 @@
+// Write a function that takes a number as argument
+// If the number is even, return true
+// Otherwise, return false
+
+function myFunction(a, b) {
+   
+    return (a/100)*b
+
+ }
